9b0210963b5fccd0f3632782ef7996e6f5b5009a
Priyanka Parikh <priyanka.parikh6@gmail.com>
2020-08-11 08:50:42 -0700

    check winner and make move by computer

--- connectfour.py
+++ connectfour.py
@@ -1,4 +1,5 @@
 import pprint
+import random
 
 class Board:
 
@@ -14,12 +15,15 @@ class Board:
         for i in range(7):
             if self.grid[i][pos] == 0:
                 self.grid[i][pos] = self.player
+                if self.winner(pos, i):
+                    print("Congratulations player {} wins".format(self.player))
                 if self.player == 1:
                     self.player = 2
                 else:
                     self.player = 1
                 return
         print("invalid entry")
+
 
     def print_board(self):
         for i in reversed(range(7)):
@@ -32,13 +36,85 @@ class Board:
                 elif self.grid[i][j] == 2:
                     line.append('O')
             line.append('|')
-            print(line)
+            print(' '.join(line))
+
+
+    def legal_moves(self):
+        """
+        Check the grid to Return a list of available moves.
+        """
+        legal_moves = []
+
+        for i in range(7):
+            if self.grid[i][-1] == 0:
+                legal_moves.append(i)
+
+        return legal_moves
+
+    def make_move(self):
+        """AI uses random to choose a number from the list of available legal moves
+        """
+        available_moves = self.legal_moves()
+        chosen_move = random.randrange(len(available_moves + 1))
+        pos = available_moves[chosen_move]
+
+
+    def winner(self, pos, i):
+        """Return if the current move at position (pos) in
+        row (i) is a winning move. This is done at each move both
+        the players make.
+
+        keyword args:
+        pos : int (posiiton of the move)
+        i : int (row number in the grid)
+        """
+        def check_vertical(pos):
+            """Check for a winner vertically
+            """
+            count = 0
+            for i in range(7):
+
+                if count == 4:
+                    return True
+                elif self.grid[i][pos] == self.player:
+                    count += 1
+                else:
+                    count = 0
+
+            return count == 4
+
+        def check_horizontal(pos, i):
+            """Check for a winner horizantally
+            """
+
+            count = 0
+
+            for move in self.grid[i]:
+                if count == 4:
+                    return True
+                elif move == self.player:
+                    count += 1
+                else:
+                    count = 0
+
+            return count == 4
+
+        def check_diagonal(pos, i):
+            pass
+
+        return check_vertical(pos) or check_horizontal(pos, i)
 
 
 game = Board()
+game.play(1)
+pprint.pprint(len(game.legal_moves()))
+game.play(1)
 game.play(2)
+game.play(2)
+game.play(3)
+game.play(3)
+pprint.pprint(len(game.legal_moves()))
 game.play(4)
-game.play(2)
 
 # pprint.pprint(game.grid)
 game.print_board()
